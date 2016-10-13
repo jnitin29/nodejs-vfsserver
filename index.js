d1472dad678cf3c113fe52ac3299726793b5a7b6
Nitin Jain <njain@walmart.com>
2016-10-12 18:47:04 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env babel-node
+
+require('./helper')
+
+async function main() {
+    // Use 'await' in here
+    console.log('main()...')
+    
+    // Your implementation here
+}
+
+main()
